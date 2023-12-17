65a4176ae5b56cd9a6a7e3dd7d25d087fc6d8095
Khalid Rabiu Salis <khalidrabiusalis@gmail.com>
2023-12-17 13:26:20 +0100

    login,signup,cart,checkout,account,out-story,contact,error_page,

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,31 @@ module.exports = {
         white_smoke: "#F5F5F5",
         dark_pastel_green: "#49BD44",
         pakistan_green: "#113A10",
+        footer_color: '#113A10',
+        input_color: '#F5F5F5',
+        
+
+
       },
+
+      borderColor: {
+        black: '#000',
+      },
+      width: {
+        919 : "919px",
+        700: "700px",
+        1200: "1200px",
+      },
+
+      height: {
+        700: "700px",
+        500: "500px",
+      },
+
+      padding:
+      {
+        
+      }
     }, // Existing Tailwind utilities
   },
   plugins: [],
